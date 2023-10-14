49130c90715049ad2493e6426e39ef5aa8cc736a
Mehdi Belfegas <mehdi.belfegas@gmail.com>
2023-10-14 17:36:19 +0200

    first commit

---
+++ DevoirStatBase/DevoirStatBase.R
@@ -0,0 +1,12 @@
+# Devoir de statistiques de base Octobre 2023
+# Groupe N 25
+
+# Question 1 : 
+tpRetinol <- read.csv2("C:/Users/Mehdi/Desktop/Statistiques/DevoirStatBase/presentationTPretinol.csv")
+summary(tpRetinol)
+
+# Question 2 : 
+
+# Question 3
+
+# Question 4
---
+++ README.md
@@ -0,0 +1,1 @@
+# M2_Statistiques_Saclay
