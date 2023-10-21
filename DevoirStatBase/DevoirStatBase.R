024c7bad25c1808a4a8d3c356dfabc67f6021da6
Mehdi Belfegas <mehdi.belfegas@gmail.com>
2023-10-21 12:37:13 +0200

    Calcul de la correlations entre toutes les variables quantitatives

--- DevoirStatBase/DevoirStatBase.R
+++ DevoirStatBase/DevoirStatBase.R
@@ -18,6 +18,7 @@
 
 library(prettyR)
 library(Epi)
+library(corrplot)
 
 # Question 1 : Décrivez vos variables
 tpRetinol <- read.csv2("C:/Users/Mehdi/Desktop/Statistiques/DevoirStatBase/presentationTPretinol.csv")
@@ -51,58 +52,44 @@ retinol_plasmatique_b <- ifelse(tpRetinol$retplasma > retinol_plasmatique_median
 #2.1 Relation entre Retinol plasmatique et age
 qqnorm(tpRetinol$retplasma)
 t.test(tpRetinol$retplasma~age_b, var.equal=TRUE)
-cor(tpRetinol$retplasma, tpRetinol$age, use="complete.obs")
 
 #2.2 Relation entre Retinol plasmatique et sexe
 t.test(tpRetinol$retplasma~tpRetinol$sexe, var.equal=TRUE)
 
 #2.3 Relation entre Retinol plasmatique et BMI
-cor(tpRetinol$retplasma, tpRetinol$sexe, use="complete.obs")
 t.test(tpRetinol$retplasma~bmi_b, var.equal=TRUE)
 
 #2.4 Relation entre Retinol plasmatique et tabac
-cor(tpRetinol$retplasma, tpRetinol$tabac, use="complete.obs")
 t.test(tpRetinol$retplasma~tabac_b, var.equal=TRUE)
 
 #2.5 Relation entre Retinol plasmatique et beta-carotene consommé
-cor(tpRetinol$retplasma, tpRetinol$betadiet, use="complete.obs")
 t.test(tpRetinol$retplasma~beta_carotene_conso_b, var.equal=TRUE)
 
 #2.6 Relation entre Retinol plasmatique et retinol consommé
-cor(tpRetinol$retplasma, tpRetinol$retdiet, use="complete.obs")
 t.test(tpRetinol$retplasma~retinol_conso_b, var.equal=TRUE)
 
 #2.7 Relation entre Retinol plasmatique et cholesterol
-cor(tpRetinol$retplasma, tpRetinol$cholesterol, use="complete.obs")
 t.test(tpRetinol$retplasma~cholesterol_b, var.equal=TRUE)
 
 #2.8 Relation entre Retinol plasmatique et alcool
-cor(tpRetinol$retplasma, tpRetinol$alcool, use="complete.obs")
 t.test(tpRetinol$retplasma~alcool_b, var.equal=TRUE)
 
 #2.9 Relation entre age et sexe
 t.test(tpRetinol$age~tpRetinol$sexe, var.equal=TRUE)
 
 #2.10 Relation entre age et BMI
-cor(tpRetinol$age, tpRetinol$bmi, use="complete.obs")
 
 #2.11 Relation entre age et tabac
-cor(tpRetinol$age, tpRetinol$tabac, use="complete.obs")
 
 #2.12 Relation entre age et beta-carotene consomme
-cor(tpRetinol$age, tpRetinol$betadiet, use="complete.obs")
 
 #2.13 Relation entre age et retinol consomme
-cor(tpRetinol$age, tpRetinol$retdiet, use="complete.obs")
 
 #2.14 Relation entre age et cholesterol
-cor(tpRetinol$age, tpRetinol$cholesterol, use="complete.obs")
 
 #2.15 Relation entre age et alcool
-cor(tpRetinol$age, tpRetinol$alcool, use="complete.obs")
 
 #2.16 Relation entre sexe et BMI
-cor(tpRetinol$sexe, tpRetinol$bmi, use="complete.obs")
 
 #2.17 Relation entre sexe et tabac
 twoby2(1-sexe_b, 1-tabac_b)
@@ -119,49 +106,40 @@ chisq.test(sexe_b, beta_carotene_conso_b)
 #2.21 Relation entre sexe et alcool
 
 #2.22 Relation entre BMI et tabac
-cor(tpRetinol$bmi, tpRetinol$tabac, use="complete.obs")
 
 #2.23 Relation entre BMI et beta-carotene consomme
-cor(tpRetinol$bmi, tpRetinol$betadiet, use="complete.obs")
 
 #2.24 Relation entre BMI et retinol consomme
-cor(tpRetinol$bmi, tpRetinol$retdiet, use="complete.obs")
 
 #2.25 Relation entre BMI et cholesterol
-cor(tpRetinol$bmi, tpRetinol$cholesterol, use="complete.obs")
 
 #2.26 Relation entre BMI et alcool
-cor(tpRetinol$bmi, tpRetinol$alcool, use="complete.obs")
 
 #2.27 Relation entre tabac et beta-carotene consomme
-cor(tpRetinol$tabac, tpRetinol$betadiet, use="complete.obs")
 
 #2.28 Relation entre tabac et retinol consomme
-cor(tpRetinol$tabac, tpRetinol$retdiet, use="complete.obs")
 
 #2.29 Relation entre tabac et cholesterol
-cor(tpRetinol$tabac, tpRetinol$cholesterol, use="complete.obs")
 
 #2.30 Relation entre tabac et alcool
-cor(tpRetinol$tabac, tpRetinol$alcool, use="complete.obs")
 
 #2.31 Relation entre beta-carotene consomme et retinol consomme
-cor(tpRetinol$betadiet, tpRetinol$retdiet, use="complete.obs")
 
 #2.32 Relation entre beta-carotene consomme et cholesterol
-cor(tpRetinol$betadiet, tpRetinol$cholesterol, use="complete.obs")
 
 #2.33 Relation entre beta-carotene consomme et alcool
-cor(tpRetinol$betadiet, tpRetinol$alcool, use="complete.obs")
 
 #2.34 Relation entre retinol consomme consomme et cholesterol
-cor(tpRetinol$retdiet, tpRetinol$cholesterol, use="complete.obs")
 
 #2.35 Relation entre retinol consomme consomme et alcool
-cor(tpRetinol$retdiet, tpRetinol$alcool, use="complete.obs")
 
 #2.36 Relation entre cholesterol et alcool
-cor(tpRetinol$cholesterol, tpRetinol$alcool, use="complete.obs")
+
+# Compute correlation
+quantitative_var <- c ("retplasma", "age", "bmi", "tabac", "betadiet", 
+                       "retdiet", "cholesterol", "alcool")
+matrix_correlation <- cor(tpRetinol[, quantitative_var], use = "complete.obs")
+corrplot(matrix_correlation, method = "circle")
 
 # Question 3 Regression lineaire avec comme variable à expliquer "retinol plasmatique concentration"
 # et les autres variables explicatives
@@ -178,9 +156,18 @@ summary(regression_linaire)
 hist(resid(regression_linaire), col="grey", main="")
 
 # Rechercher synergies
-data_intereset <- tpRetinol[, c("retplasma", "age", "sexe", "bmi", "tabac",
+data_interest <- tpRetinol[, c("retplasma", "age", "sexe", "bmi", "tabac",
                                 "betadiet", "retdiet", "cholesterol", "alcool")]
-regression_lineaire_synergies <- lm(data_intereset$retplasma ~ .^2, data=data_intereset )
+regression_lineaire_synergies <- lm(data_interest$retplasma ~ .^2, data=data_interest)
+tt <- lm(data_interest$retplasma ~ (data_interest$age
+                                     +data_interest$sexe
+                                     +data_interest$bmi
+                                     +data_interest$tabac
+                                     +data_interest$betadiet
+                                     +data_interest$retdiet
+                                     +data_interest$cholesterol
+                                     +data_interest$alcool)^2
+                                      , data=data_interest)
 
 # Question 4 Regression logistique
 regression_logistique <- glm(retinol_plasmatique_b~tpRetinol$age
@@ -199,7 +186,7 @@ exp(confint(regression_logistique)) # calcul des intervalles de confiance
 drop1(regression_logistique,.~.,test="Chisq")
 
 # Rechercher synergies
-data_interest_logicstic <- data_intereset
+data_interest_logicstic <- data_interest
 data_interest_logicstic$retplasma <- retinol_plasmatique_b
 regression_logistique_synergies <- glm(data_interest_logicstic$retplasma ~ .^2, 
                                       data=data_interest_logicstic,
